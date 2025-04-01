2337a943d8edf7d87766a21b7bf6735892e5b4e1
Stephanie Baard <stephanie.baard@gmail.com>
2025-04-01 16:24:10 +0200

    Added counter

--- src/routes/FlowerPage.tsx
+++ src/routes/FlowerPage.tsx
@@ -1,9 +1,23 @@
+import React, { useState } from "react";
 
 const FlowerPage = () => {
+    const [count, setCount] = useState(0);
+
+    const add = () => {
+        setCount(count + 1);
+    };
+
+    const imageSrc = count >= 10 ? "../../../images/FoxWithBlushEyesClosed.png" : "../../../images/Fox.png";
+
     return (
       <div>
-        <h1>Flower Page</h1>
-        <p>Add flower counter</p>
+        <div>
+        <img className="Flower" src="../../../images/FlowerWithStem.png" alt="Flower" />
+        <img src = {imageSrc} alt="Fox" className="Fox"/>
+        </div>
+        <img className="Flower" src="../../../images/FlowerWithStem.png" alt="Flower" />
+        <h1>{count}</h1>
+        <button onClick={add}>Give flower</button>
       </div>
     );
   };
